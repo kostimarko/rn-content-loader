30f3ef05c4ed44c87d4d904c4650f91a47dd2aa5
Karthik-B-06 <karthik06051996@gmail.com>
2020-10-21 23:38:11 +0530

    feat: Added type definitions

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+declare module 'rn-content-loader' {
+  import React from 'react'
+
+  interface ContentLoaderProps {
+    primaryColor: string;
+    secondaryColor: string;
+    duration: number;
+    width: string | number;
+    height: string | number;
+    viewBox: string;
+    x1: string;
+    y1: string;
+    x2: string;
+    y2: string;
+  }
+
+
+  const ContentLoader: React.FC<ContentLoaderProps>;
+
+  export default ContentLoader;
+}
