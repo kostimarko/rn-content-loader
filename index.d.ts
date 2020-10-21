43d5d9aaa8e95fbf166e054899fc6cbea55be01e
Karthik-B-06 <karthik06051996@gmail.com>
2020-10-21 23:50:21 +0530

    feat: Marked all props as not required

--- index.d.ts
+++ index.d.ts
@@ -2,16 +2,16 @@ declare module 'rn-content-loader' {
   import React from 'react'
 
   interface ContentLoaderProps {
-    primaryColor: string;
-    secondaryColor: string;
-    duration: number;
-    width: string | number;
-    height: string | number;
-    viewBox: string;
-    x1: string;
-    y1: string;
-    x2: string;
-    y2: string;
+    primaryColor?: string;
+    secondaryColor?: string;
+    duration?: number;
+    width?: string | number;
+    height?: string | number;
+    viewBox?: string;
+    x1?: string;
+    y1?: string;
+    x2?: string;
+    y2?: string;
   }
 
 
